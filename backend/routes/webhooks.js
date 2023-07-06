77c73b46f669cc9c9b3ded1f7a69a3476cdc8414
3loka <trilok.ramakrishna@gmail.com>
2023-07-06 16:05:18 +0530

    bunch of changes

--- backend/routes/webhooks.js
+++ backend/routes/webhooks.js
@@ -4,44 +4,30 @@ const Sequelize = require('sequelize');
 const { DataTypes } = Sequelize;
 
 module.exports = function(sequelize) {
-    const Issue = sequelize.define('Issue', {
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true
-        },
-        description: DataTypes.STRING,
-        severity: DataTypes.STRING,
-        created: {
-          type: DataTypes.DATE,
-          defaultValue: Sequelize.NOW
-        }
-      }, {
-        timestamps: false,
-        tableName: 'issues',
-        freezeTableName: true
-      });
       
+      const Issue = sequelize.define('Issue', {
+          id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+          },
+          description: DataTypes.STRING,
+          severity: DataTypes.STRING,
+          created: {
+            type: DataTypes.DATE,
+            defaultValue: Sequelize.NOW
+          },
+          meta: {
+            type: DataTypes.JSON,
+          }
+        }, {
+          timestamps: false,
+          tableName: 'issues',
+          freezeTableName: true
+        });
+  
       
 
   let webhookUrl;
-
-//   let cache = {
-//     User1: {
-//         hcp_jwt: "dummyToken1",
-//         orgid: "fc064bc9-fc9d-41ee-9e0d-11fb39e059a5",
-//         projectid: "dummyProjectId1",
-//         url: "http://ec2-44-204-175-78.compute-1.amazonaws.com:28081",
-//         installationToken: "3"
-//     },
-//     User2: {
-//       hcp_jwt: "dummyToken1",
-//       orgid: "fc064bc9-fc9d-41ee-9e0d-11fb39e059a5",
-//       projectid: "dummyProjectId1",
-//       url: "http://ec2-44-204-175-78.compute-1.amazonaws.com:28081",
-//       installationToken: "3"
-//     }
-// };
-
 
   const router = express.Router();
 
@@ -95,15 +81,21 @@ module.exports = function(sequelize) {
       res.status(400).json({ message: 'Invalid username or password' });
     }
   });
-  
 
+router.post('/events', authenticate, async (req, res) => {
+  const event = req.body.payload;
 
-  router.post('/events', authenticate, async (req, res) => {
-      const event = req.body;
-  console.log('Received event:', event);
+  // Segregate the values for the Issue model and the meta field.
+  const { installation_id, organization_id, project_id, sender, payload: { id, description, severity } } = event;
+  const meta = { installation_id, organization_id, project_id, sender };
+
+  // We no longer include these values when building the issue object.
+  const issueData = { id, description, severity, meta: JSON.stringify(meta) };
+
+  console.log('Received event:', issueData);
 
   try {
-      const issue = Issue.build(event);
+      const issue = Issue.build(issueData);
       await issue.save();
       console.log('Saved issue:', issue.toJSON());
       res.status(200).end();
@@ -112,6 +104,7 @@ module.exports = function(sequelize) {
       res.status(500).json({ message: 'Error saving issue' });
   }
 });
+
 
 
 
@@ -161,70 +154,6 @@ module.exports = function(sequelize) {
 
     res.status(200).json({ message: 'Token, OrgID and ProjectID stored successfully' });
 });
-
-
-
-// router.get('/clearCache', (req, res) => {
-//   // Clear the cache
-//   cache['hcp_jwt'] = null;
-//   cache['orgid'] = null;
-//   cache['projectid'] = null;
-//   cache['url'] = null;
-
-//   res.status(200).json({ message: 'Cache cleared successfully' });
-// });
-
-
-
-// //   router.get('/consulVersions', async (req, res) => {
-// //     // const authHeader = req.headers['authorization'];
-// //     // const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
-// //     // console.log(token)
-// //     // console.log(process.env.UI_TOKEN)
-
-// //     // if (token == null || token !== process.env.UI_TOKEN) return res.sendStatus(401); // if there isn't a token in the request, or it's not the expected one, return 401 Unauthorized
-
-// //     const url = `${process.env.HCP_URL}/global-network-manager/2022-02-15/organizations/${process.env.HCP_ORG}/integration/3?from_app=true`;
-
-// //     try {
-// //         const response = await axios.get(url, {
-// //             headers: { 'Authorization': `Bearer ${process.env.HCP_JWT}` }
-// //         });
-// //         res.json(response.data);
-// //     } catch (err) {
-// //         console.error('Error getting consul versions:', err);
-// //         res.status(500).json({ message: 'Error getting consul versions' });
-// //     }
-// // });
-
-// router.get('/services', async (req, res) => {
-//   const url = `${cache['orgid']}/global-network-manager/2022-02-15/organizations/${cache['orgid']}/projects/${cache['projectid']}/services?filter.kinds=KIND_TYPICAL&filter.kinds=KIND_MESH_GATEWAY&filter.kinds=KIND_INGRESS_GATEWAY&from_app=true`;
-
-//   try {
-//       const response = await axios.get(url, {
-//           headers: { 'Authorization': `Bearer ${cache['hcp_jwt']}` }
-//       });
-//       res.json(response.data);
-//   } catch (err) {
-//       console.error('Error getting consul versions:', err);
-//       res.status(500).json({ message: 'Error getting consul versions' });
-//   }
-// });
-
-
-
-
-// router.post('/storeData', (req, res) => {
-//   const { token, orgid, projectid, url, username } = req.body;
-//   if (!token || !orgid || !projectid || !username) {
-//       return res.status(400).json({ message: 'Token, OrgID, ProjectID and Username are required' });
-//   }
-  
-//   // Store the token, OrgID and ProjectID in the cache under the username
-//   cache[username] = { token, orgid, projectid, url };
-
-//   res.status(200).json({ message: 'Data stored successfully' });
-// });
 
 router.get('/services', async (req, res) => {
   const { username } = req.query;
